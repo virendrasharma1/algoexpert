389c0015a51f42e5eefb4a2d513f501111f7d2d9
unknown <virendra.sharma0211@gmail.com>
2023-10-09 21:36:50 +0530

    monotonic array - Arrays

---
+++ arrays/monotonicArray.java
@@ -0,0 +1,56 @@
+package arrays;
+
+/*
+Time - O(n) as while loop is O(n) and next loop also O(1)
+Space - O(1) as integers are constanr
+ */
+public class monotonicArray {
+
+    public static boolean isMonotonic(int[] array) {
+        if (array.length == 0 || array.length == 1) return true;
+        int firstElement = array[0];
+        int length = array.length - 1;
+        int lastElement = array[length];
+
+        if (firstElement == lastElement) {
+            while (lastElement == firstElement && length > 0) {
+                lastElement = array[length];
+                length--;
+            }
+        }
+
+        if (firstElement == lastElement) return true;
+
+        String isIncreasingOrDecreasing = "";
+
+        if (firstElement > lastElement) {
+            isIncreasingOrDecreasing = "DECREASING";
+        } else {
+            isIncreasingOrDecreasing = "INCREASING";
+
+        }
+        int prevElementValue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            int element = array[i];
+            if (isIncreasingOrDecreasing.equals("DECREASING")) {
+                if (prevElementValue >= element) {
+                    prevElementValue = element;
+                } else {
+                    return false;
+                }
+            } else {
+                if (prevElementValue <= element) {
+                    prevElementValue = element;
+                } else {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isMonotonic(new int[]{1, 1, 1, 1, 1, 1}));
+    }
+}
